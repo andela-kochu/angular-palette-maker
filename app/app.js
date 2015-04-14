84ff4fb6669915bb0b01c7aa94d01886df59ad3d
Kingsley Ochu <andela-kochu@github.com>
2015-04-14 15:10:46 +0100

    added Preview Feature

--- app/app.js
+++ app/app.js
@@ -2,25 +2,36 @@
 
 /* App Module */
 
-var paletteMakerApp = angular.module('paletteMakerApp', [/*
-  'ngRoute',
-  'paletteMakerControllers',
-  'paletteMakerFilters'
-*/]);
+var paletteMakerApp = angular.module('paletteMakerApp', ['ngMaterial']);
 
-/*paletteMakerApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/phones', {
-        templateUrl: 'partials/phone-list.html',
-        controller: 'PhoneListCtrl'
-      }).
-      when('/phones/:phoneId', {
-        templateUrl: 'partials/phone-detail.html',
-        controller: 'PhoneDetailCtrl'
-      }).
-      otherwise({
-        redirectTo: '/phones'
-      });
-  }]);
-*/
+paletteMakerApp.factory('colorPalette', function(){
+    return {
+      hue: 145,
+      saturation: 10,
+      lightness: 20
+    };
+  })
+  .factory('colorCollection', function(){
+    return [];
+  })
+
+/**
+ * filters
+ * ----------------------------------------------
+ */
+
+  /**
+   * hexacolor
+   *
+   * transform an object {r:255, v:255, b:255} to hexa #FFFFFF
+   * @return string 
+   */
+  .filter('hexacolor', function () {
+  return function (color) {
+    var output = "#";
+    output += color.r < 16 ? '0' + color.r.toString(16) : color.r.toString(16);
+    output += color.g < 16 ? '0' + color.g.toString(16) : color.g.toString(16);
+    output += color.b < 16 ? '0' + color.b.toString(16) : color.b.toString(16);
+    return output.toUpperCase();
+  };
+});
--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -4,24 +4,46 @@
 
 
 
-paletteMakerApp.controller('paletteMakerController', ['$scope', '$http',
-  function($scope, $http) {/*
-    $http.get('phones/phones.json').success(function(data) {*/
-      $scope.phones = "data";
-   /* });
 
-    $scope.orderProp = 'age';*/
-  }]);
 
-/*phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http',
-  function($scope, $routeParams, $http) {
-    $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
-      $scope.phone = data;
-      $scope.mainImageUrl = data.images[0];
-    });
+paletteMakerApp.controller('paletteMakerController', ['$scope', 'colorPalette', function($scope, colorPalette){
+  $scope.createColor = function(){
+  }
+    
+  $scope.hue = colorPalette.hue;
+  $scope.saturation = colorPalette.saturation;
+  $scope.lightness = colorPalette.lightness;
+  $scope.colorData = [];
+  $scope.createColor = function() {
+    colorPalette.hue = parseInt($scope.hue, 10);
+    console.log(colorPalette.hue)
+    colorPalette.saturation = parseInt($scope.saturation, 10);
+    colorPalette.lightness=  parseInt($scope.lightness, 10);
+    $scope.colorData.push({hue:  colorPalette.hue, saturation:  colorPalette.saturation,  lightness:  colorPalette.lightness});
+  };
+  $scope.avatarData = [{
+        id: "avatars:svg-1",
+        title: 'avatar 1',
+        value: 5
+      },{
+        id: "avatars:svg-2",
+        title: 'avatar 2',
+        value: 100
+      },{
+        id: "avatars:svg-3",
+        title: 'avatar 3',
+        value: 250
+    }];
+    
+    $scope.submit = function() {
+      alert('submit');
+    };
+    $scope.addItem = function() {
+      var r = Math.ceil(Math.random() * 255);
+      $scope.avatarData.push({ title: r, value: r });
+    };
+    $scope.removeItem = function() {
+      $scope.colorData.pop();
+    };
 
-    $scope.setImage = function(imageUrl) {
-      $scope.mainImageUrl = imageUrl;
-    }
-  }]);
-*/
+}]);
