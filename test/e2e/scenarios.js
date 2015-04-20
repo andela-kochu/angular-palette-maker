c3ab13fdbeec1b982b7bb5e3aacbe5127c113c98
Kingsley Ochu <andela-kochu@github.com>
2015-04-20 14:10:38 +0100

    added e2e test

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,11 +2,41 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('Palette Maker App', function() {
 
-  it('should redirect index.html to index.html#/', function() {
-    browser.get('');
-    expect(browser.getLocationAbsUrl()).toBe('/');
-      });
+describe('Palettemaker App', function() {
+
+  browser.get('#/anyother');
+
+  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
+    expect(browser.getLocationAbsUrl()).toMatch("/home");
+  });
+
+  describe('Home View', function() {
+
+    beforeEach(function() {
+      browser.get('#/home');
+    });
+    it('should open the landing page', function() {
+    expect(browser.getLocationAbsUrl()).toMatch("/home");
+    });
+    it('should expect the name of link to match CLick', function() {
+      expect(element(by.css("#palette")).getText()).toMatch("Click");
+    });
+  });
+
+  describe('Palette View', function() {
+
+    beforeEach(function() {
+      browser.get('#/palette');
+    });
+    it('should open the main palette page', function() {
+    expect(browser.getLocationAbsUrl()).toMatch("/palette");
+    });
+    it('should get the name of the buttons', function() {
+      expect(element(by.binding("addUpdateBtn")).getText()).
+        toMatch("ADD COLOR");
+      expect(element(by.binding("editCancelBtn")).getText()).
+        toMatch("EDIT COLOR");
+    });
   });
 });
